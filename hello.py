7fb88a99ba068d54913104c5d3f20854bffdedf5
John Lee <jhlee90741@gmail.com>
2023-09-24 17:06:42 -0400

    added extra functionality

--- hello.py
+++ hello.py
@@ -16,32 +16,50 @@ class NameForm (FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()]) 
     email = StringField('What is your UofT Email Address?', validators=[Email()]) 
     submit = SubmitField('Submit')
- 
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    uoft = 'mail.utoronto.ca'
-    uoft_val = None #uoft email validation variable: none if not uoft, 1 if uoft
+    uoft = 'utoronto'
+    uoft_val = None #uoft email validation variable: none if not uoft, 1 if uoft  
+    first = None
+
+    if 'name' not in session:
+        session['name'] = None
+
+    if 'email' not in session:
+        session['email'] = None
+
+    if 'first' not in session:
+        first = True
+    else:
+        first = session.get('first')
 
     form = NameForm()    
     if form.validate_on_submit():
         old_name = session.get('name')
         old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
         if old_email is not None and old_email != form.email.data:
             flash('Looks like you have changed your email!')
         session['name'] = form.name.data
-        if uoft in form.email.data:
-            session['email'] = form.email.data
-        else:
+        session['email'] = form.email.data
+        if uoft not in form.email.data:
             flash('Please enter a valid UofT email')
+
+        session['first'] = False
+
         return redirect(url_for('index'))
+   
     if session['email'] is not None and uoft in session['email']:
         uoft_val = 1
     else:
         uoft_val = 0
-    return render_template('index.html', form = form, name = session.get('name'), uoft_val = uoft_val, email = session.get('email'))
+
+    return render_template('index.html', form = form, name = session.get('name'), uoft_val = uoft_val, email = session.get('email'), first = first)
 
 @app.route('/user/<name>')
 def user(name):
