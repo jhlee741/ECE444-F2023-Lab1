1ef6a692784df5654bef714d8471f42990306979
John Lee <jhlee90741@gmail.com>
2023-09-23 20:04:15 -0400

    partly finished part 2 of activity 4

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -16,14 +16,20 @@ class NameForm (FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()]) 
     submit = SubmitField('Submit')
 
+class EmailForm (FlaskForm): 
+    name = StringField('What is your UofT Email Address?', validators=[Email()]) 
+    submit = SubmitField('Submit')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None 
-    form = NameForm() 
-    if form.validate_on_submit(): 
-        name = form.name.data 
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
